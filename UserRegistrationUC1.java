0380fb18cc8687cee3375c53f6327b51742d9a66
himanshunete <himanshuneteh@gmail.com>
2021-01-15 07:58:43 +0530

    ADDED: functionality to Enter Valid First Name

--- UserRegistrationUC1.java
+++ UserRegistrationUC1.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class UserRegistrationUC1{
    public static void main(String[] args){
-       System.out.print("Welcome to User Registration");
+      Scanner enterFirstName = new Scanner(System.in);
+
+      PATTERN_FOR_VALID_FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$"
+      System.out.println("Enter Valid First Name");
+      String firstName = enterFirstName.nextLine();
+
+      if ( firstName ==  PATTERN_FOR_VALID_FIRST_NAME ) {
+         System.out.println(" first Name is valid ");
+      else
+         System.out.println("  first Name is not valid ");
+      }
+
    }
 }
+
+
+
