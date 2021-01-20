720e1937d8b920b31c22fb0ffa72e53b9940f7af
himanshunete <himanshuneteh@gmail.com>
2021-01-20 10:28:21 +0530

    MODIFIED: functionality to throw custom exceptions for Invalid

--- UserRegistration/src/test/java/com/samples/UserValidatorTest.java
+++ UserRegistration/src/test/java/com/samples/UserValidatorTest.java
@@ -2,7 +2,7 @@ package com.samples;
 
 import org.junit.Assert ;
 import org.junit.Test ;
-
+import org.junit.rules.ExpectedException;
 
 public class UserValidatorTest {
 
@@ -14,10 +14,16 @@ public class UserValidatorTest {
     }
     
     @Test
-    public void givenFirstName_WhenShort_ShouldReturnFalse() {
+    public void givenFirstName_WhenInvalid_ShouldReturnFalse() {
         UserValidator validator = new UserValidator();
-        boolean result = validator.validateFirstName("Hi");
-        Assert.assertEquals(false, result);
+        try {
+			ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(UserValidationException.class);
+            boolean result = validator.validateFirstName("Hi");
+            Assert.assertEquals(true, result);
+        } catch (UserValidationException e) {
+        	Assert.assertEquals("Hi", e.getMessage());
+        }
     }
     
     @Test
@@ -28,10 +34,37 @@ public class UserValidatorTest {
     }
     
     @Test
+    public void givenLastName_WhenInvalid_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        try {
+			ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(UserValidationException.class);
+            boolean result = validator.validateLastName("Ne");
+            Assert.assertEquals(true, result);
+        } catch (UserValidationException e) {
+        	Assert.assertEquals("Ne", e.getMessage());
+        }
+    }
+    
+    
+    @Test
     public void givenEmail_WhenValid_ShouldReturnTrue() {
         UserValidator validator = new UserValidator();
         boolean result = validator.validateEmailAddress("himanshuneteh@gmail.com");
         Assert.assertEquals(true, result);
+    }
+    
+    @Test
+    public void givenEmailAddress_WhenInvalid_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        try {
+			ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(UserValidationException.class);
+            boolean result = validator.validateEmailAddress("Himshu@1.com.com.com");
+            Assert.assertEquals(true, result);
+        } catch (UserValidationException e) {
+        	Assert.assertEquals("Himshu@1.com.com.com", e.getMessage());
+        }
     }
     
     @Test
@@ -42,10 +75,36 @@ public class UserValidatorTest {
     }
     
     @Test
+    public void givenMobileNumber_WhenInvalid_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        try {
+			ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(UserValidationException.class);
+            boolean result = validator.validateMobileNumber("91 45135");
+            Assert.assertEquals(true, result);
+        } catch (UserValidationException e) {
+        	Assert.assertEquals("91 45135", e.getMessage());
+        }
+    }
+    
+    @Test
     public void givenPassword_WhenValid_ShouldReturnTrue() {
         UserValidator validator = new UserValidator();
         boolean result = validator.validatePassword("Himanshuetefs@12345");
         Assert.assertEquals(true, result);
+    }
+    
+    @Test
+    public void givenPassword_WhenInvalid_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        try {
+			ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(UserValidationException.class);
+            boolean result = validator.validatePassword("Nete1234");
+            Assert.assertEquals(true, result);
+        } catch (UserValidationException e) {
+        	Assert.assertEquals("Nete1234", e.getMessage());
+        }
     }
     
    
