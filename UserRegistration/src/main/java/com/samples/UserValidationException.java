4b55acabb9e89071b2fe431db59ea3952a93b7a4
himanshunete <68182639+himanshunete@users.noreply.github.com>
2021-01-21 11:59:36 +0530

    Delete UserRegistration directory

--- UserRegistration/src/main/java/com/samples/UserValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.samples;
-
-public class UserValidationException extends RuntimeException {
-	public UserValidationException(String ffirstname,String flastname,String femail,String fmobilenumber,String fpassword ) {
-       super(flastname);
-    }
-}
-
